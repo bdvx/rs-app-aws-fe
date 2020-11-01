435bc67209d337fafa1fe4086ed891e382fced9b
Bdvx <bdv2507@gmail.com>
2020-11-02 00:36:11 +0300

    feat: change data to dynamic

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 
 const API_PATHS = {
-  product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev'
+  product: 'https://755vvfmegj.execute-api.eu-west-1.amazonaws.com/dev',
+  order: 'https://755vvfmegj.execute-api.eu-west-1.amazonaws.com/dev',
+  import: 'https://755vvfmegj.execute-api.eu-west-1.amazonaws.com/dev',
+  bff: 'https://755vvfmegj.execute-api.eu-west-1.amazonaws.com/dev'
 };
 
 export default API_PATHS;
