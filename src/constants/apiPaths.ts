3e6b6b9bee760c7c49d8a754c313d7be34798b9c
Denis_Beskorovainyi <besdenv@gmail.com>
2020-11-17 05:59:09 +0300

    fix: imports and links

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,7 +2,7 @@
 const API_PATHS = {
   product: 'https://755vvfmegj.execute-api.eu-west-1.amazonaws.com/dev',
   order: 'https://755vvfmegj.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://755vvfmegj.execute-api.eu-west-1.amazonaws.com/dev',
+  import: 'https://77upjvo4f6.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://755vvfmegj.execute-api.eu-west-1.amazonaws.com/dev'
 };
 
